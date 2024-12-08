1faae65a9c1aac0f93eb3d06bee7ea084125a42b
trepz <tr3ppy@gmail.com>
2024-12-08 22:06:02 +1000

    attempt impl

--- grammar.js
+++ grammar.js
@@ -10,8 +10,55 @@
 module.exports = grammar({
   name: "swarm_object",
 
+  extras: $ => [/\s/],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => $.pair,
+
+    val: $ => choice(
+      $.object,
+      $.array,
+      $.string,
+      $.number,
+      $.true,
+      $.false,
+    ),
+
+    object: $ => seq('{', $.pair, '}'),
+
+    array: $ => seq('[', optional($.val_list), ']'),
+
+    val_list: $ => seq($.val, repeat(seq(',', $.val))),
+
+    pair: $ => choice($.key_val, $.type),
+
+    key_val: $ => seq(
+      choice($.string, $.key_string, $.prop),
+      '=',
+      $.val
+    ),
+
+    type: $ => seq('__type', '=', choice($.string, $.null)),
+
+    key_string: $ => repeat(/[a-zA-Z][a-zA-Z0-9_]/),
+
+    string: $ => seq(
+      '"', 
+      repeat(choice(
+        /[^"\\]/, 
+        /\\(["\\bfnrt])/
+      )),
+      '"'
+    ),
+
+    prop: $ => seq('__', $.key_string),
+
+    number: $ => /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/,
+
+    true: $ => 'True',
+
+    false: $ => 'False',
+
+    null: $ => '"null"'
   }
 });
